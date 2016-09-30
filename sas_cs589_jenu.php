fda0a6c3cf4eefc0b838e1ddd20add44bab9e97c
shahjenu <shahjenu@gmail.com>
2016-09-29 17:35:27 -0700

    Add files via upload

---
+++ sas_cs589_jenu.php
@@ -0,0 +1,35 @@
+<!doctype html>
+<html>
+    <head>
+            <meta http-equiv="content-type" content="text/html; charset ="utf-8" />
+        <Title> This is my first web page in the title</Title>
+        
+    </head>
+
+    <body>
+    
+     	<h1> Hi, Welcome to my website </h1>
+        	<p>This is my SAS work web page</p>
+        	<p> Hope you could find some of achievement here.</p>
+    	<p>This is my picture</p>
+<img src = "F:\Pics after CA\personal pics\J6.jpg" width = 10% height = 10% alt = "What do you think about me">
+	
+    <br>
+    	
+		<a href = "E:\Personal_Jenu\SEM-3\SAS\FinalWork\CS589_A_WEEK6_16219.docx"> Week6</a><br>	
+		<a href = "E:\Personal_Jenu\SEM-3\SAS\FinalWork\CS589_A_WEEK7_16219.docx"> Week7</a><br>	
+		<a href = "E:\Personal_Jenu\SEM-3\SAS\FinalWork\CS589_A_WEEK8_16219.docx"> Week8</a><br>	
+		<a href = "E:\Personal_Jenu\SEM-3\SAS\FinalWork\CS589_A_Midterm_16219.docx"> Week9_Midterm</a>	<br>
+		<a href = "E:\Personal_Jenu\SEM-3\SAS\FinalWork\CS589_A_WEEK10_16219.docx"> Week10</a>	<br>
+		<a href = "E:\Personal_Jenu\SEM-3\SAS\FinalWork\CS589_A_WEEK11_16219.docx"> Week11</a><br>	
+		<a href = "E:\Personal_Jenu\SEM-3\SAS\FinalWork\CS589_A_WEEK12_16219.docx"> Week12</a><br>	
+		<a href = "E:\Personal_Jenu\SEM-3\SAS\FinalWork\CS589_A_WEEK13_16219.docx"> Week13</a><br>	
+		
+		
+		<script type='text/javascript' src='https://www.google.com/jsapi'></script>  
+    </body>
+
+</html>
+
+
+
